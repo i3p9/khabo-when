bada72a15aac1e6d387ab8915bd948cff2a675f0
i3p9 <i3p9@users.noreply.github.com>
2022-04-05 23:39:01 +0600

    feat: add seheri timer

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
-const iftarTimeDhaka = [1648988340, 1649074740, 1649161200, 1649247600, 1649334060, 1649420460, 1649506860, 1649593320, 1649679720, 1649766180, 1649852580, 1649938980, 1650025440, 1650111840, 1650198240, 1650284700, 1650371100, 1650457560, 1650543960, 1650630420, 1650716820, 1650803280, 1650889680, 1650976140, 1651062540, 1651148940, 1651235400, 1651321800, 1651408260, 1651494660]
+//const iftarTimeDhaka = [1648988340, 1649074740, 1649161200, 1649247600, 1649334060, 1649420460, 1649506860, 1649593320, 1649679720, 1649766180, 1649852580, 1649938980, 1650025440, 1650111840, 1650198240, 1650284700, 1650371100, 1650457560, 1650543960, 1650630420, 1650716820, 1650803280, 1650889680, 1650976140, 1651062540, 1651148940, 1651235400, 1651321800, 1651408260, 1651494660];
+//const seheriTimeDhaka = [1648938420,1649024760,1649111040,1649197440,1649283780,1649370120,1649456460,1649542800,1649629140,1649715480,1649801820,1649888160,1649974500,1650060840,1650147180,1650233520,1650319860,1650406200,1650492540,1650578880,1650665220,1650751560,1650837900,1650924240,1651010580,1651096920,1651183260,1651269600,1651355940,1651442280];
+
+const iftarTimeDhaka = [1648988340, 1649074740, 1649161200, 1649247600, 1649334060];
+const seheriTimeDhaka = [1648938420, 1649024760, 1649111040, 1649197440, 1649283780, 1649370120];
+
 function timeConverter(UNIX_timestamp) {
-    var a = new Date(UNIX_timestamp * 1000);
-    var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-    var year = a.getFullYear();
-    var month = months[a.getMonth()];
-    var date = a.getDate();
-    var hour = a.getHours();
-    var min = a.getMinutes();
-    var sec = a.getSeconds();
-    var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min;// + ':' + sec ;
+    let a = new Date(UNIX_timestamp * 1000);
+    let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    let year = a.getFullYear();
+    let month = months[a.getMonth()];
+    let date = a.getDate();
+    let hour = a.getHours();
+    let min = a.getMinutes();
+    let sec = a.getSeconds();
+    let time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min;// + ':' + sec ;
     return time;
 }
 
 function timeConverterDateOnly(UNIX_timestamp) {
-    var a = new Date(UNIX_timestamp * 1000);
-    var months = ['Jan', 'Feb', 'Mar', 'April', 'May', 'June', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-    var year = a.getFullYear();
-    var month = months[a.getMonth()];
-    var date = a.getDate();
-    var hour = a.getHours();
-    var min = a.getMinutes();
-    var sec = a.getSeconds();
-    var time = `${date} ${month} ${year}`
+    let a = new Date(UNIX_timestamp * 1000);
+    let months = ['Jan', 'Feb', 'Mar', 'April', 'May', 'June', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    let year = a.getFullYear();
+    let month = months[a.getMonth()];
+    let date = a.getDate();
+    let hour = a.getHours();
+    let min = a.getMinutes();
+    let sec = a.getSeconds();
+    let time = `${date} ${month} ${year}`
     return time;
 }
 
@@ -33,53 +38,88 @@ let todaysIftar;
 let ramadanDate;
 
 for (let index = 0; index < iftarTimeDhaka.length; index++) {
-    console.log(`we took ${index} now.`)
+    console.log(`iftar: we took ${index} now.`)
     if (currentTime > iftarTimeDhaka[index]) {
         console.log(index);
         todaysIftar = iftarTimeDhaka[index + 1];
         //console.log(iftarTimeDhaka[index + 1]);
-        ramadanDate = index +2;
-    } else{
+        ramadanDate = index + 2;
+    } else {
         break;
     }
 }
 
-if(ramadanDate == 3){
-    ramadanDate = '3rd';
-} else if(ramadanDate == 1){
-    ramadanDate = '1st';
-} else {
-    ramadanDate = ramadanDate + 'nd';
+//handle seheri
+for (let index = 0; index < seheriTimeDhaka.length; index++) {
+    console.log(`seheri: we took ${index} now.`)
+    if (currentTime > seheriTimeDhaka[index]) {
+        console.log(index);
+        todaysSeheri = seheriTimeDhaka[index + 1];
+        //console.log(iftarTimeDhaka[index + 1]);
+        ramadanDate = index + 2;
+    } else {
+        break;
+    }
 }
+let showIftar;
+
+//decide what to show??
+if (todaysIftar > todaysSeheri) {
+    console.log(`iftar bigger than seheri`);
+    //show seheri
+    showIftar = false;
+} else {
+    console.log(`seheri bigger than iftar`);
+    //shwo iftar
+    showIftar = true;
+}
+
+console.log(showIftar);
 
 
 console.log(`Today's Iftar time is ${timeConverter(todaysIftar)}`);
 
 
-var remainingTime = 1649161200;
-//var haha = '<span id="haha"></span>';
-var countDownSpan = document.getElementById('timer');
-var ramadanDateSpan = document.getElementById('ramadanDate');
-var todayDateSpna = document.getElementById('todayDate');
+let remainingTime = 1649161200;
+//let haha = '<span id="haha"></span>';
+let countDownSpan = document.getElementById('timer');
+let ramadanDateSpan = document.getElementById('ramadanDate');
+let todayDateSpna = document.getElementById('todayDate');
+let iftarOrSeheriSpan = document.getElementById('iftarOrSeheri');
 todayDateSpna.innerHTML = `${timeConverterDateOnly(currentTime)}`;
 
 let newInterval;
-newInterval = setInterval(function () { MModeTimer(todaysIftar+300) }, 1000);
+if (showIftar == true) {
+    newInterval = setInterval(function () { MModeTimer(todaysIftar + 300) }, 1000);
+    iftarOrSeheriSpan.innerHTML = 'iftar';
+} else {
+    newInterval = setInterval(function () { MModeTimer(todaysSeheri + 300) }, 1000);
+    iftarOrSeheriSpan.innerHTML = 'seheri ends';
+    ramadanDate -= 1;
+};
+
+if (ramadanDate == 3) {
+    ramadanDate = '3rd';
+} else if (ramadanDate == 1) {
+    ramadanDate = '1st';
+} else {
+    ramadanDate = ramadanDate + 'th';
+}
 
 
 function MModeTimer(todaysIftar) {
 
-    var currentTime = new Date().getTime() / 1000;
-    var futureTime = todaysIftar;
-    var timeRemaining = futureTime - currentTime;
-    var minute = 60;
-    var hour = 60 * 60;
-    var day = 60 * 60 * 24;
-    var dayFloor = Math.floor(timeRemaining / day);
-    var hourFloor = Math.floor((timeRemaining - dayFloor * day) / hour);
-    var minuteFloor = Math.floor((timeRemaining - dayFloor * day - hourFloor * hour) / minute);
-    var secondFloor = Math.floor((timeRemaining - dayFloor * day - hourFloor * hour - minuteFloor * minute));
-    var countdownCompleted = "EAT NOW!!!";
+    let currentTime = new Date().getTime() / 1000;
+    let futureTime = todaysIftar;
+    let timeRemaining = futureTime - currentTime;
+    let minute = 60;
+    let hour = 60 * 60;
+    let day = 60 * 60 * 24;
+    let dayFloor = Math.floor(timeRemaining / day);
+    let hourFloor = Math.floor((timeRemaining - dayFloor * day) / hour);
+    let minuteFloor = Math.floor((timeRemaining - dayFloor * day - hourFloor * hour) / minute);
+    let secondFloor = Math.floor((timeRemaining - dayFloor * day - hourFloor * hour - minuteFloor * minute));
+    let countdownCompleted = "EAT NOW!!!";
 
     if (secondFloor <= 0 && minuteFloor <= 0) {
         window.location.reload(true);
@@ -89,22 +129,30 @@ function MModeTimer(todaysIftar) {
     } else {
 
         if (futureTime > currentTime) {
-            var content = `${hourFloor} Hours ${minuteFloor} Minutes ${secondFloor} Seconds`;
+            let content = `${hourFloor} Hours ${minuteFloor} Minutes ${secondFloor} Seconds`;
             countDownSpan.innerHTML = content;
             ramadanDateSpan.innerHTML = ramadanDate;
         }
     }
 }
 
-function changeLocation(){
+function changeLocation() {
     let locationSpan = document.getElementById('location');
-    if(locationSpan.innerHTML === 'jhd'){
+    if (locationSpan.innerHTML === 'jhd') {
         locationSpan.innerHTML = 'dhk';
         clearInterval(newInterval);
-        newInterval = setInterval(function () { MModeTimer(todaysIftar) }, 1000);
+        if (showIftar == true) {
+            newInterval = setInterval(function () { MModeTimer(todaysIftar) }, 1000);
+        } else {
+            newInterval = setInterval(function () { MModeTimer(todaysSeheri) }, 1000);
+        };
     } else {
         locationSpan.innerHTML = 'jhd';
         clearInterval(newInterval);
-        newInterval = setInterval(function () { MModeTimer(todaysIftar+300) }, 1000);
+        if (showIftar == true) {
+            newInterval = setInterval(function () { MModeTimer(todaysIftar + 300) }, 1000);
+        } else {
+            newInterval = setInterval(function () { MModeTimer(todaysSeheri + 300) }, 1000);
+        };
     }
 }
